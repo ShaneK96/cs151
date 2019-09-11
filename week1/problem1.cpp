b43f5098e2e94ae70a9bccdf694b7f3512affc6b
ShaneK96 <shaneheadley96@gmail.com>
2019-09-10 20:14:03 -0400

    Sep 10

---
+++ week1/problem1.cpp
@@ -0,0 +1,16 @@
+// Name: Shane Farnell
+// Date: 08/29/2019
+// Assignment: Problem1
+
+// Imports extra library in order to print to the screen
+// this is a pre-processor directive
+
+#include<iostream> 
+
+using namespace std;
+//every C++ program MUST have 
+// at least one function that function name is main()
+int main() // this line is the header 
+{   this line starts the body
+    cout << "Hello World \n";
+}   // this line ends the body of main ()   
---
+++ week3/number.cpp
@@ -0,0 +1,16 @@
+// this program has an integer variable named number
+// date: sep 10 2019
+// name: Shane Farnell
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    int number;
+    number = 5;
+    cout << "the value of the number is: ";
+    cout << number;
+    cout << "\n";
+    return 0;
+}
